fed64d338e7b873885ca60f7843cf12c33a2bfa3
martinezmanuela <martinez.manuela.22@gmail.com>
2015-10-03 13:18:17 -0300

    arreglada matrize

--- src/pags_web.cpp
+++ src/pags_web.cpp
@@ -9,7 +9,7 @@ void resolver_pags_web(
         case ALG_PAGERANK: {
             matrize data(ifile, cant_nodos, cant_aristas);
             vector<double> inicial (cant_nodos, (double) 1/cant_nodos);
-            cout << inicial[cant_nodos - 1] << endl;
+            //cout << inicial[cant_nodos - 1] << endl;
             resultado = data.potencias(inicial, args.c, args.tol);
             break;
         }
@@ -27,50 +27,50 @@ matrize::matrize(ifstream& data, int cant_nodos, int cant_aristas) {
     _cant_nodos = cant_nodos;
     int i = 0;
     int j = 0;
-    vector<vector<int> > dataPorFila(_cant_nodos, vector<int>());
+    vector<vector<int> > dataPorColumna(_cant_nodos, vector<int>());
 
     for (int k = 0; k < cant_aristas; k++) {
         data >> i;
         i--;
         data >> j;
         j--;
-        dataPorFila[i].push_back(j);
+        dataPorColumna[j].push_back(i);
         //cout << i << "," << j << endl; DEBUG
     }
 
-    for (int i = 0; i < dataPorFila.size(); i++) {
-        sort(dataPorFila[i].begin(), dataPorFila[i].end());
+    for (int i = 0; i < dataPorColumna.size(); i++) {
+        sort(dataPorColumna[i].begin(), dataPorColumna[i].end());
     }
 
 
     for (int l = 0; l < _cant_nodos; l++) {
-        if (dataPorFila[l].size() == 0) {
-            ptr_filas.push_back(-1);
+        if (dataPorColumna[l].size() == 0) {
+            ptr_cols.push_back(-1);
         } else {
-            for (int h = 0; h < dataPorFila[l].size(); h++) {
-                int n = dataPorFila[l].size();
+            ptr_cols.push_back(vals.size());
+            for (int h = 0; h < dataPorColumna[l].size(); h++) {
+                int n = dataPorColumna[l].size();
                 
                 vals.push_back((double) 1 / n);
-                ind_cols.push_back(dataPorFila[l][h]);
+                ind_filas.push_back(dataPorColumna[l][h]);
             };
-            ptr_filas.push_back(vals.size());
         };
     }
 
-    /*for (int i = 0; i < vals.size(); i++) {
+    for (int i = 0; i < vals.size(); i++) {
         cout << vals[i] << ",";
     }
     cout << endl << endl;
 
-    for (int i = 0; i < ind_cols.size(); i++) {
-        cout << ind_cols[i] << ",";
+    for (int i = 0; i < ind_filas.size(); i++) {
+        cout << ind_filas[i] << ",";
     }
     cout << endl << endl;
 
-    for (int i = 0; i < ptr_filas.size(); i++) {
-        cout << ptr_filas[i] << ",";
+    for (int i = 0; i < ptr_cols.size(); i++) {
+        cout << ptr_cols[i] << ",";
     }
-    cout << endl << endl;  DEBUG*/   
+    cout << endl << endl;  //DEBUG    
 }
 
 vector<double> matrize::potencias(const vector<double>& inicial, double c, double tol) const {
@@ -104,7 +104,7 @@ vector<double> matrize::prod(const vector<double>& vec, double c) const {
         } else {
             acum = dumping;  // arranco con el valor de dumping
             for (int j = ptr_actual; j < ptr_next; j++) { // vamos sumando el valor correspondiente a cada posición no nula de la fila
-                acum = acum + (1 - c) * vals[j] * vec[ind_cols[j]];
+                acum = acum + (1 - c) * vals[j] * vec[ind_filas[j]];
             }
         }
         res.push_back(acum);
@@ -114,11 +114,11 @@ vector<double> matrize::prod(const vector<double>& vec, double c) const {
 }
 
 void matrize::rango_fila(int fila, int& ptr_actual, int& ptr_next) const {
-    ptr_actual = ptr_filas[fila];
+    ptr_actual = ptr_cols[fila];
     if (ptr_actual != -1) {
         ptr_next = -1;
         for (int j = fila + 1; j < _cant_nodos && ptr_next == -1 ; j++) {
-            ptr_next = ptr_filas[j];
+            ptr_next = ptr_cols[j];
         }
         if (ptr_next == -1) {
             ptr_next = _cant_nodos;
@@ -133,7 +133,7 @@ double matrize::difManhattan(const vector<double>& v1, const vector<double>& v2)
     for (int i = 0; i < v1.size(); i++) {
         res = res + abs(v1[i] - v2[i]);
     }
-    cout << res << endl;
+    //cout << res << endl;
     return res;
 }
 
@@ -156,9 +156,9 @@ vector<double> indeg(ifstream& ifile, int cant_nodos, int cant_aristas) {
     }
 
 
-    for (int i = 0; i < resultado.size(); i++) {
+    /*for (int i = 0; i < resultado.size(); i++) {
         cout << resultado[i] << ",";
     }
-    cout << endl << endl; 
+    cout << endl << endl; */
     return resultado;
 }
