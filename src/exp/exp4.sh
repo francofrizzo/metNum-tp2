8488977378b95fda45fb3600e6e66e306e3574d4
Lucía Rabinowicz <lu.rabinowicz@gmail.com>
2015-10-05 22:45:36 -0300

    iban dos manchas caminando y una se cae y le grita a la otra AYUDIN AYUDINN. ya esat el experimento 4. BESIIIIIIS

--- src/exp/exp4.sh
+++ src/exp/exp4.sh
@@ -4,11 +4,12 @@ cs=0.85 #c probabilidad de teletransportación
 nodos=13
 iteraciones=1
 tolerancia=0.00001
+algoritmo=0
 
-
-while getopts 'cha:' opt; do
+while getopts 'chal:' opt; do
   case $opt in
     a) iteraciones=$OPTARG ;;
+    l) algoritmo=$OPTARG ;;
     h) echo ""
        echo "    Experimento 2. Calcula el tiempo de ejecución cuando variamos la "
        echo "    cantidad de nodos."
@@ -17,6 +18,7 @@ while getopts 'cha:' opt; do
        echo "        -c        Elimina los archivos generados por el experimento."
        echo "        -h        Imprime este texto de ayuda."
        echo "        -a <núm>  Cantidad de iteraciones- por defecto 1"
+       echo "        -l        Si es 1, corre con indeg. Si es 0 corre con Page Rank.-por defecto PageRank"
        echo ""
        exit 0 ;;
     c) if [ -d $(dirname $0)/exp4 ]; then rm $(dirname $0)/exp4 -R; fi
@@ -35,20 +37,35 @@ fi
 echo "Generando datos de entrada...";
 mkdir -p $(dirname $0)/exp4 #crear carpeta
 
-#for k in $(seq 1 2); do
-  #python $(dirname $0)/../tools/webparser.py $(dirname $0)/../tools/weblist-exp4-$k.in $(dirname $0)/exp4/exp4-graph-$k.out
   python $(dirname $0)/../tools/webparser.py $(dirname $0)/../tools/weblist-exp4-1.in $(dirname $0)/exp4/exp4-graph-1.out
-#done
 
+if [[ $algoritmo == 0 ]] ;then
+  echo "algoritmo: Page Rank"
 
-for j in $(seq $iteraciones); do 
-  #for i in $(seq 1 2); do
-      #$(dirname $0)/../tp 0 $cs 0 $(dirname $0)/exp4/exp4-graph-$i.out $tolerancia -r $(dirname $0)/exp4/exp4-ranking-$i.out -t -o $(dirname $0)/exp4/exp4-$i.out|
-       $(dirname $0)/../tp 0 $cs 0 $(dirname $0)/exp4/exp4-graph-1.out $tolerancia -r $(dirname $0)/exp4/exp4-ranking-1.out -t -o $(dirname $0)/exp4/exp4-1.out|
+  for j in $(seq $iteraciones); do 
+      
+      $(dirname $0)/../tp 0 $cs 0 $(dirname $0)/exp4/exp4-graph-1.out $tolerancia -r $(dirname $0)/exp4/exp4-ranking-1-$algoritmo.out -t -o $(dirname $0)/exp4/exp4-1-$algoritmo.out|
+      
+      $(dirname $0)/../tp 0 $cs 0 $(dirname $0)/exp4-graph-2.out $tolerancia -r $(dirname $0)/exp4/exp4-ranking-2-$algoritmo.out -t -o $(dirname $0)/exp4/exp4-2-$algoritmo.out|
 
-  sed 's/.*: //' |
+      sed 's/.*: //' |
       while IFS= read -r line; do
         printf " %d \n" "$line" >> $(dirname $0)/exp4/exp4-tiempos.txt ;
       done
-  #done
-done
+  done
+
+else
+  echo "algoritmo: indeg"
+  
+  for j in $(seq $iteraciones); do 
+      
+      $(dirname $0)/../tp 1 $cs 0 $(dirname $0)/exp4/exp4-graph-1.out $tolerancia -r $(dirname $0)/exp4/exp4-ranking-1-$algoritmo.out -t -o $(dirname $0)/exp4/exp4-1-$algoritmo.out|
+      
+      $(dirname $0)/../tp 1 $cs 0 $(dirname $0)/exp4-graph-2.out $tolerancia -r $(dirname $0)/exp4/exp4-ranking-2-$algoritmo.out -t -o $(dirname $0)/exp4/exp4-2-$algoritmo.out|
+
+      sed 's/.*: //' |
+      while IFS= read -r line; do
+        printf " %d \n" "$line" >> $(dirname $0)/exp4/exp4-tiempos.txt ;
+      done
+  done
+fi
